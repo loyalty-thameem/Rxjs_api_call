c6a4d74373aa1ce6d4ac5ed2ab76c5e3a9405762
Thameem Ansari <97792702+loyalty-thameem@users.noreply.github.com>
2023-02-10 11:44:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { interval } from "rxjs";
+import { switchMap } from "rxjs/operators";
+const App = ({ userId }) => {
+  const [data, setData] = useState(null);
+  // const userId = 1;
+
+  useEffect(() => {
+    const interval$ = interval(2000).pipe(
+      switchMap(() => {
+        return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+          .then((response) => response.json())
+          .then((json) => {
+            setData(json);
+          });
+      })
+    );
+
+    const subscription = interval$.subscribe();
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [userId]);
+
+  return (
+    <div>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
+};
+
+export default App;
